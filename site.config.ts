fdc16b47b1decaafd5ca8c4e072b82bb21c20376
NCC-17039 <120711536+Bashir-1@users.noreply.github.com>
2023-09-13 15:49:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '70fb4a90cd83486290a2bf845db08a7f',
+  rootNotionPageId: '17539b870e3748a689391dab5f307ded',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'NCC-17039',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'NCC-17039's English Blog.',
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
